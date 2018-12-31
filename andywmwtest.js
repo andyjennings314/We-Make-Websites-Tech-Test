062b5b5dd07dda1c3b72f7cc272cb6077b7d1609
andyjennings314 <andyjennings314@gmail.com>
2018-12-31 13:01:44 +0000

    Initialised AngularJS

--- andywmwtest.js
+++ andywmwtest.js
@@ -1,1 +1,16 @@
-// JavaScript Document
+(function () {
+    var app = angular.module('AJWMW', []);
+
+	var homeCtrl = function ($scope, $rootScope, $timeout) {
+        var self = this;
+		
+		self.pageTitle = "Women's Jeans";
+		self.pageDescription = "Our perfected jeans are designed with every body type in mind. With a comfortable mid rise and skinny leg, ours hugs your curves in all the right places for a universally flattering fit.";
+		self.products = [
+						 {title: "Lexy Super Skinny", price: "£128.00", onSale: true},
+						 ];
+
+    }
+
+    app.controller('homeCtrl', homeCtrl);
+}());
