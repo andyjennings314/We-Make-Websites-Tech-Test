27b5189423d012b4a8d85cf622bd3f47e4e6d049
andyjennings314 <andyjennings314@gmail.com>
2018-12-31 17:01:50 +0000

    Better use of currency $filter

--- andywmwtest.js
+++ andywmwtest.js
@@ -1,16 +1,16 @@
 (function () {
     var app = angular.module('AJWMW', []);
 
-	var homeCtrl = function ($scope, $rootScope, $timeout) {
+	var homeCtrl = function ($filter) {
         var self = this;
 		
 		self.pageTitle = "Women's Jeans";
 		self.pageDescription = "Our perfected jeans are designed with every body type in mind. With a comfortable mid rise and skinny leg, ours hugs your curves in all the right places for a universally flattering fit.";
 		self.products = [
-						 {title: "Lexy Super Skinny", price: "£128.00", onSale: false, image: "test1.jpg"},
-						 {title: "Kendra Straight Leg in Rinse Supersoft", price: "£128.00", onSale: false, image: "test2.jpg"},
-						 {title: "Kendra Straight Leg in Deep Super Soft", price: "£128.00", onSale: false, image: "test3.jpg"},
-						 {title: "Alissa Super Skinny in Dark Supersoft", price: "£98.00", onSale: true, image: "test4.jpg"},
+						 {title: "Lexy Super Skinny", price: 128, onSale: false, image: "test1.jpg"},
+						 {title: "Kendra Straight Leg in Rinse Supersoft", price: 128, onSale: false, image: "test2.jpg"},
+						 {title: "Kendra Straight Leg in Deep Super Soft", price: 128, onSale: false, image: "test3.jpg"},
+						 {title: "Alissa Super Skinny in Dark Supersoft", price: 98, onSale: true, image: "test4.jpg"},
 						 ];
 
     }
